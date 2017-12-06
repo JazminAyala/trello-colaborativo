7fd66f8acfdf8b1119adcf0c638b769cdb584267
Laawe <laura.render.03@gmail.com>
2017-12-06 09:22:51 -0600

    Actualizando index.html// app.js

--- js/app.js
+++ js/app.js
@@ -1,50 +1,36 @@
 /* add.EventListener nos permite indicar al agente de usuario que permanezca atento a la interacción
 de un usuario sobre un elemento en concreto, sin necesidad de tocar un sólo caracter de nuestro marcado. */
-window.addEventListener('load', function () {   /* para la carga */
-    var boxAddList = document.getElementsByClassName('add-list')[0];  /* se llama a la clase desde HTML */
+var container = document.createElement('div');
+var parentSection = document.getElementById('container');
 
-    boxAddList.addEventListener('click', function () {  /* para cuando el usuario haga clik */
+window.addEventListener('load', firstBox )  /* para la carga */
+    var boxAddList = document.getElementById('add-list');
+  /* se llama a la clase desde HTML */
 
-        /*creando etiquetas ya declaradas en CSS */
-        var container = document.createElement('div');
-        var form = document.createElement('input')
-        var btnSave = document.createElement('button');
-        var btnClose = document.createElement('i');
-        var parentSection = document.getElementById('container')   /* se llama al id del HTML llamado "container" */
+function firstBox(){
+  boxAddList.addEventListener("click", createBox);
+}
+function createBox(){
+  var input = document.createElement('input');
+  var btnSave = document.createElement('button');
+  var btnClose = document.createElement('i');
 
-        /* Agregando elementos SECTION-TRES del html */
-        /* appendChild, agregará  un nuevo nodo al final de la lista de un elemento hijo de un elemento padre especificado */
-        container.appendChild(form);
-        container.appendChild(btnSave);
-        container.appendChild(btnClose);
-        parentSection.appendChild(container);
+  container.appendChild(input);
+  container.appendChild(btnSave);
+  container.appendChild(btnClose);
+  parentSection.appendChild(container);
 
-        /* Agregando Atributos SECTION-TRES del html */
-        /* setAttribute, agregará un nuevo atributo */
-        container.setAttribute('class', 'box-form');
-        form.setAttribute('placeholder', 'Añadir una lista...');
-        form.setAttribute('class', 'input-style');
-        btnSave.setAttribute('class', 'btn-style');
-        btnSave.setAttribute('id','save-js');
-        btnClose.setAttribute('class','fa fa-times btn-close-style');
+  container.setAttribute('class', 'box-form');
+  input.setAttribute('placeholder', 'Añadir una lista...');
+  input.setAttribute('class', 'input-style');
+  btnSave.setAttribute('class', 'btn-style');
+  btnSave.setAttribute('id','save-js');
+  btnClose.setAttribute('class','fa fa-times btn-close-style');
+  btnSave.textContent = 'Guardar';
 
-        /*agregando texto al botón color verde de la caja desplegable */
-        btnSave.textContent = 'Guardar';   /* btnSave elemento agregado */
+  btnSave.addEventListener("click", buttonSave);
+}
 
-        /*Borrar al div inicial */
-        /* removeChild elimina un nodo hijo del DOM y puede devolver el nodo eliminado.*/
-        parentSection.removeChild(boxAddList);
-
-        /* Creando Evento del botón guardar color verde de la ventana desplegable */
-        btnSave.addEventListener('click',function(){
-            var textList= form.value;
-
-            /** Creando la nueva caja desplegable */
-            var containerList = document.createElement('div');
-            var titleList = document.createElement('p');
-            var boxInput = document.createElement('p');
-    })
-
-  })
-
-})
+function buttonSave(){
+  createBox();
+}
